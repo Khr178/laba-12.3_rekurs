de5fd2a43d2289aa0b58c5a7c699a4775e8be052
Khr178 <115107723+Khr178@users.noreply.github.com>
2023-04-14 15:48:07 +0300

    Add files via upload
    first commit

---
+++ юніт тест 12.3. rekurs.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 12.3 рекурс. спосіб/лаба 12.3 рекурс. спосіб.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест123rekurs
+{
+	TEST_CLASS(юніттест123rekurs)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            Node* head = NULL;
+            Node* tail = NULL;
+            appendNode(&head, &tail, 1);
+            appendNode(&head, &tail, 2);
+            appendNode(&head, &tail, 3);
+            appendNode(&head, &tail, 4);
+            appendNode(&head, &tail, 4); // дубль елемента
+            appendNode(&head, &tail, 5);
+
+            bool hasDup = hasDuplicate(head);
+            Assert::IsTrue(hasDup);
+        }
+
+        TEST_METHOD(TestNoDuplicate)
+        {
+            Node* head = NULL;
+            Node* tail = NULL;
+            appendNode(&head, &tail, 1);
+            appendNode(&head, &tail, 2);
+            appendNode(&head, &tail, 3);
+            appendNode(&head, &tail, 4);
+            appendNode(&head, &tail, 5);
+
+            bool hasDup = hasDuplicate(head);
+            Assert::IsFalse(hasDup);
+		}
+	};
+}
